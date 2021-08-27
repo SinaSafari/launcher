250454f5da4b03984e9f9c480dee8681b0c1262e
sinasafari <m.sinasafari@gmail.com>
2021-08-27 08:44:42 +0430

    base auth exception handling

--- app/exceptions/auth_exceptions.py
+++ app/exceptions/auth_exceptions.py
@@ -6,7 +6,4 @@ from fastapi_jwt_auth.exceptions import AuthJWTException
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": exc.message}
-    )
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.message})
--- config/auth.py
+++ config/auth.py
@@ -4,4 +4,6 @@ from pydantic import BaseModel
 
 
 class AuthSettings(BaseModel):
-    authjwt_secret_key = os.getenv("JWT_SECRET", "dc5f32dd22dd0b294912d46c787f09491213fe264873c002d446b776132c1284")
+    authjwt_secret_key = os.getenv(
+        "JWT_SECRET", "dc5f32dd22dd0b294912d46c787f09491213fe264873c002d446b776132c1284"
+    )
