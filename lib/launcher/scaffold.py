a191152cdf7a32255831157efc2bdd3b452fb1f5
sinasafari <m.sinasafari@gmail.com>
2021-08-27 18:36:50 +0430

    add style for cli (termcolor)

--- lib/launcher/scaffold.py
+++ lib/launcher/scaffold.py
@@ -1,5 +1,6 @@
 import os
 from typing import Callable
+from termcolor import colored
 
 APP_BASE_PATH = f"{os.getcwd()}/app"
 MODELS_PATH = f"{APP_BASE_PATH}/models"
@@ -8,6 +9,13 @@ SERVICES_PATH = f"{APP_BASE_PATH}/services"
 CONTROLLERS_PATH = f"{APP_BASE_PATH}/controllers"
 API_PATH = f"{APP_BASE_PATH}/api/v1"
 
+msg_colors = {
+    "error": "red",
+    "warning": "yellow",
+    "success": "green",
+    "info": "grey",
+}
+
 
 def write_to_file(filename: str, content: str):
     try:
@@ -15,7 +23,11 @@ def write_to_file(filename: str, content: str):
             f.write(content)
     except FileExistsError:
         print(
-            f"a file with provided name already exists. {filename} can't be recreated."
+            colored(
+                f"a file with provided name already exists. {filename} can't be recreated.",
+                msg_colors["error"],
+                attrs=["bold"],
+            )
         )
         exit(1)
 
@@ -24,7 +36,13 @@ def delete_file(filename):
     try:
         os.remove(filename)
     except FileNotFoundError:
-        print(f"{filename} \nfile not found. please check filesystem manually.")
+        print(
+            colored(
+                f"{filename} \nfile not found. please check filesystem manually.",
+                msg_colors["error"],
+                attrs=["bold"],
+            )
+        )
         exit(1)
 
 
@@ -74,16 +92,39 @@ def create_gql_resolver(name: str):
 
 def scaffold_printer(func: Callable, instance_name, create=True):
     if create:
-        print(f"⌛ creating {instance_name}...")
+
+        print(colored(f"⌛ creating {instance_name}...", msg_colors["info"]))
         func(instance_name)
-        print(f"{instance_name} created successfully.")
+        print(colored(f"{instance_name} created successfully.", msg_colors["success"]))
     else:
-        print(f"⌛ deleting {instance_name}...")
+        print(colored(f"⌛ deleting {instance_name}...", msg_colors["info"]))
         func(instance_name)
-        print(f"{instance_name} \n✅deleted successfully.")
+        print(
+            colored(f"{instance_name} \n✅deleted successfully.", msg_colors["success"])
+        )
 
 
 def scaffold(name: str) -> bool:
+    """
+    this function used for adding regular files for an entity in the accepted launcher web app architecture.
+    also these files will be filled with some auto generated boilerplate, like some necessary imports and class
+    or function names
+
+    if anythings went wrong, warning or error messages will be shown in the stdout and the app will be exited.
+    by the way, it's not revert (or rollback) the operation, so check the file system manually. in future versions
+    it could be changed.
+
+    Todo add rollback feature
+
+    Parameters
+    ----------
+    name
+        name of the entity that will be used in name of files and class or function names,
+
+    Returns
+    -------
+    bool: indicate that everything goes well.
+    """
     if not name:
         print("⚠️ name is required")
         exit(1)
@@ -96,13 +137,24 @@ def scaffold(name: str) -> bool:
     # todo: uncomment this when the structure has been created.
     # scaffold_printer(create_gql_resolver, name)
 
-    print("✔️ scaffolding was successful.")
+    print(
+        colored("✔️ scaffolding was successful.", msg_colors["success"], attrs=["bold"])
+    )
     return True
 
 
 def descaffold(name: str) -> bool:
+    """
+    this function used for removing all the generated file using scaffold (-g in cli)
+
+    Parameters
+    ----------
+    name
+        exact name of entity that used in scaffolding
+
+    """
     if not name:
-        print("⚠️ name is required")
+        print(colored("⚠️ name is required", msg_colors["error"]))
         exit(1)
 
     scaffold_printer(delete_file, f"{MODELS_PATH}/{name}.py", create=False)
@@ -116,5 +168,6 @@ def descaffold(name: str) -> bool:
     scaffold_printer(delete_file, f"{API_PATH}/{name}_api.py", create=False)
 
     # todo: graphql support should be added later
-    print("deleted successfully")
+
+    print(colored("deleted successfully", msg_colors["success"], attrs=["bold"]))
     return True
